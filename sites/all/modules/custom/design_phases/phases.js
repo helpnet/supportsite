c99500d6e03e34b1919b355390c1c0dc33a43652
vagrant <jas.blanchard@gmail.com>
2013-02-08 18:36:01 +0000

    Added js to face in design overviews

--- sites/all/modules/custom/design_phases/phases.js
+++ sites/all/modules/custom/design_phases/phases.js
@@ -1,2 +1,31 @@
 (function($) {
+  Drupal.behaviors.designPhases = {
+    attach: function(context, settings) {
 
+      var menuLinks;
+
+      $('.node-design-phase-description').hide();
+
+      menuLinks = $('.pane-menu-menu-design-model-test-menu li.leaf a');
+
+      $.each(menuLinks, function(key, value) {
+
+        var node = $(value).attr('id');
+
+        var nodeDiv = $('div#' + node);
+
+        $(value).click( function() {
+
+          $('.node-design-phase-description').hide();
+
+          $(nodeDiv).fadeIn('fast'); 
+
+          return false;
+        });
+
+      });
+
+
+    }
+  }
+})(jQuery);
