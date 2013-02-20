b26a8653bc941572b0b7fa1bfe841ec00f6f5e00
jasonblanchard <jas.blanchard@gmail.com>
2013-02-20 14:14:06 -0500

    Expanding working

--- sites/all/modules/custom/design/design_tasks.js
+++ sites/all/modules/custom/design/design_tasks.js
@@ -2,7 +2,18 @@
   Drupal.behaviors.designTasks = {
     attach: function(context, settings) {
 
-      console.log($('.views-row'));
+      $('.field-name-field-design-phase-task-overview').hide();
+      $('.field-name-field-related-job-aids-view').hide();
+      $('.field-name-field-related-teaching-online-vi').hide();
+      
+      $('.design-phase-task-row .field-name-title').click(function() {
+
+        $(this).parents('.design-phase-task-row').find('.field-name-field-design-phase-task-overview').slideToggle('fast');
+
+        $(this).parents('.design-phase-task-row').find('.field-name-field-related-job-aids-view').toggle('fast');
+
+        $(this).parents('.design-phase-task-row').find('.field-name-field-related-teaching-online-vi').toggle('fast');
+      });
 
     }
   }
