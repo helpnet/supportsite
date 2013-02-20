1513c6397c587ef2e9d88c42972ad278146110f7
jasonblanchard <jas.blanchard@gmail.com>
2013-02-20 15:00:31 -0500

    Added an expand all link

--- sites/all/modules/custom/design/design_tasks.js
+++ sites/all/modules/custom/design/design_tasks.js
@@ -2,9 +2,29 @@
   Drupal.behaviors.designTasks = {
     attach: function(context, settings) {
 
+
+      $('.pane-design-phase-tasks .pane-title').after("<a href='#'class='expand-all'>Expand All</a>");
+
+      $('a.expand-all').click(function() {
+
+        $('.field-name-field-design-phase-task-overview').slideToggle('fast');
+        $('.field-name-field-related-job-aids-view').slideToggle('fast');
+        $('.field-name-field-related-teaching-online-vi').slideToggle('fast');
+
+        return false;
+      });
+
       $('.field-name-field-design-phase-task-overview').hide();
       $('.field-name-field-related-job-aids-view').hide();
       $('.field-name-field-related-teaching-online-vi').hide();
+
+      var titleElement = $('.design-phase-task-row .field-name-title h3');
+
+      titleElement.each(function(index, value) {
+        var title = $(value).text();
+
+        $(value).html("<a href='#'>" + title + "</a>");
+      });
       
       $('.design-phase-task-row .field-name-title').click(function() {
 
@@ -13,6 +33,8 @@
         $(this).parents('.design-phase-task-row').find('.field-name-field-related-job-aids-view').toggle('fast');
 
         $(this).parents('.design-phase-task-row').find('.field-name-field-related-teaching-online-vi').toggle('fast');
+
+        return false;
       });
 
     }
