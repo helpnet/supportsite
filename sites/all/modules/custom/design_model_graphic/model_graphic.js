9e949b5953239c4366eb3e5687cfa2a357353455
jasonblanchard <jas.blanchard@gmail.com>
2013-02-26 11:25:35 -0500

    Abstracted design phase model into a function

--- sites/all/modules/custom/design_model_graphic/model_graphic.js
+++ sites/all/modules/custom/design_model_graphic/model_graphic.js
@@ -2,12 +2,12 @@
   Drupal.behaviors.designModelGraphic = { 
     attach: function(context, settings) {
 
-      var planLink = $('.menu li').children()[0];
-
+      // Create the paper canvas from a DOM element.
       var paperDiv = $('.pane-main-menu .pane-content')[0];
 
       paper = Raphael(paperDiv, 600, 300);
 
+      // Create the middle bubble.
       var model = paper.circle(paper.width * .30, paper.height * .5, 60);
 
       model.attr("fill", "rgb(213, 73, 73)");
@@ -16,22 +16,37 @@
       modelLabel = paper.text(model.attrs.cx, model.attrs.cy, "Design\nModel");
       modelLabel.attr("font", "20px 'Lucida Grande'");
 
-      var planPhase = paper.circle(paper.width * .50, paper.height * .15, 40);
-      planPhase.attr("fill", "rgb(129, 179, 157)");
-      planPhase.attr('href', '#');
+      // Get the menu links.
+      var planLink = $('.menu li').children()[0];
 
-      planLabel = paper.text(planPhase.attrs.cx, planPhase.attrs.cy, planLink.text);
-      planLabel.attr("font", "20px", "Lucida Grande");
-      planLabel.attr('href', '#');
+      // Function to add the bubbles.
+      function createPhaseBubble(link, xval, yval) {
 
-      planPhase.click(function() {
-        planLink.click();
-      });
+        var phase = paper.circle(xval, yval, 40);
 
-      planLabel.click(function() {
-        planLink.click();
-      });
+        phase.attr("fill", "rgb(129, 179, 157)");
+        phase.attr('href', '#');
 
+        label = paper.text(phase.attrs.cx, phase.attrs.cy, link.text);
+        label.attr("font", "20px", "Lucida Grande");
+        label.attr('href', '#');
+
+        $(label.node).click(function() {
+          link.click();
+          return false;
+        });
+
+        $(phase.node).click(function() {
+          link.click();
+          return false; 
+        });
+
+        line = paper.path('M' + model.attrs.cx + ' ' +  model.attrs.cy + 'L' + phase.attrs.cx + ' ' + phase.attrs.cy)
+        line.toBack();
+      }
+
+      // Create a bubble for each phase.
+      createPhaseBubble(planLink, paper.width * .50, paper.height * .15, 40);
 
     }
   }
